af73345089b17932927834b9f344822411f3ce26
OmkarGeedh1 <72271706+OmkarGeedh1@users.noreply.github.com>
2020-10-03 02:10:44 +0530

    Update auth.js

--- mern-ecom-backend/routes/auth.js
+++ mern-ecom-backend/routes/auth.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const { check } = require("express-validator");
 const { signout, signup, signin } = require("../controllers/auth");
@@ -16,6 +17,47 @@ router.post(
     check("password")
       .isLength({ min: 5 })
       .withMessage("password should be of minimum length 5")
+  ],
+  signup
+);
+
+router.post(
+  "/signin",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("valid email is required"),
+    check("password")
+      .isLength({ min: 1 })
+      .withMessage("password field is required")
+  ],
+  signin
+);
+
+router.get("/signout", signout);
+
+module.exports = router;
+*/
+
+//declare expressJS
+const express = require("express");
+const { check } = require("express-validator");
+const { signout, signup, signin } = require("../controllers/auth");
+
+const router = express.Router();
+
+router.post(
+  "/signup",
+  [
+    check("name")
+      .isLength({ min: 3 })
+      .withMessage("name should be at minimum 3 character long"),
+    check("email")
+      .isEmail()
+      .withMessage("valid email is required"),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("password should be of minimum length 6")
   ],
   signup
 );
